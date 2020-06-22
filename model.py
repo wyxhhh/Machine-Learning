9fbc53dea9110bfeb57215a1cc788773ba7cf91c
wyx <wyx_ei@sjtu.edu.cn>
2020-06-22 10:38:04 +0800

    gitignore

--- model.py
+++ model.py
@@ -76,19 +76,6 @@ class GatedAttention(nn.Module):
         self.D = 128
         self.K = 1
 
-        # self.feature_extractor_part1 = nn.Sequential(
-        #     nn.Conv2d(1, 20, kernel_size=5),
-        #     nn.ReLU(),
-        #     nn.MaxPool2d(2, stride=2),
-        #     nn.Conv2d(20, 50, kernel_size=5),
-        #     nn.ReLU(),
-        #     nn.MaxPool2d(2, stride=2)
-        # )
-
-        # self.feature_extractor_part2 = nn.Sequential(
-        #     nn.Linear(50 * 4 * 4, self.L),
-        #     nn.ReLU(),
-        # )
         self.fc = nn.Sequential(
             nn.Linear(2048, self.L),
             nn.ReLU(),
@@ -106,53 +93,12 @@ class GatedAttention(nn.Module):
 
         self.attention_weights = nn.Linear(self.D, self.K)
 
-        self.classifier1 = nn.Sequential(
-            nn.Linear(self.L*self.K, 1),
-            nn.Sigmoid()
-        )
-        self.classifier2 = nn.Sequential(
-            nn.Linear(self.L*self.K, 1),
-            nn.Sigmoid()
-        )
-        self.classifier3 = nn.Sequential(
-            nn.Linear(self.L*self.K, 1),
-            nn.Sigmoid()
-        )
-        self.classifier4 = nn.Sequential(
-            nn.Linear(self.L*self.K, 1),
-            nn.Sigmoid()
-        )
-        self.classifier5 = nn.Sequential(
-            nn.Linear(self.L*self.K, 1),
-            nn.Sigmoid()
-        )
-        self.classifier6 = nn.Sequential(
-            nn.Linear(self.L*self.K, 1),
-            nn.Sigmoid()
-        )
-        self.classifier7 = nn.Sequential(
-            nn.Linear(self.L*self.K, 1),
-            nn.Sigmoid()
-        )
-        self.classifier8 = nn.Sequential(
-            nn.Linear(self.L*self.K, 1),
-            nn.Sigmoid()
-        )
-        self.classifier9 = nn.Sequential(
-            nn.Linear(self.L*self.K, 1),
-            nn.Sigmoid()
-        )
-        self.classifier10 = nn.Sequential(
-            nn.Linear(self.L*self.K, 1),
-            nn.Sigmoid()
-        )
+        self.proj = nn.Linear(self.L*self.K, 10)
 
     def forward(self, x):
         x = x.squeeze(0)
-
-        # H = self.feature_extractor_part1(x)
-        # H = H.view(-1, 50 * 4 * 4)
-        # H = self.feature_extractor_part2(H)  # NxL
+        x = x.squeeze(1)
+        
         H = self.fc(x)
 
         A_V = self.attention_V(H)  # NxD
@@ -160,46 +106,38 @@ class GatedAttention(nn.Module):
         A = self.attention_weights(A_V * A_U) # element wise multiplication # NxK
         A = torch.transpose(A, 1, 0)  # KxN
         A = F.softmax(A, dim=1)  # softmax over N
-
         M = torch.mm(A, H)  # KxL
-
-        prob = []
-        prob.append(self.classifier1(M))
-        prob.append(self.classifier2(M))
-        prob.append(self.classifier3(M))
-        prob.append(self.classifier4(M))
-        prob.append(self.classifier5(M))
-        prob.append(self.classifier6(M))
-        prob.append(self.classifier7(M))
-        prob.append(self.classifier8(M))
-        prob.append(self.classifier9(M))
-        prob.append(self.classifier10(M))
-        Y_prob = np.array(prob)
-        # Y_hat = torch.ge(Y_prob, 0.5).float()
-        Y_hat = np.zeros(10)
-        for i in range(len(Y_hat)):
-            Y_hat[i] = torch.ge(Y_prob[i], 0.5).float()
-        return Y_prob, Y_hat, A
+        out = self.proj(M)
+        return torch.sigmoid(out)
 
 
     # AUXILIARY METHODS
     def calculate_classification_error(self, X, Y):
         # Y = Y.float()
-        _, Y_hat, _ = self.forward(X)
+        Y_prob = self.forward(X)[0]
+        Y = Y[0]
+        Y_hat = []
+        # p = np.sum(np.array(Y))
+        # print(p)
+        for i in range(len(Y_prob)):
+            Y_hat.append(torch.ge(Y_prob[i], 0.5).float())
         error = 0
         for i in range(len(Y_hat)):
-            # print(type(Y[i]))
-            error += 1. - Y_hat[i].eq(Y[i].float).cpu().float().mean().data[0]
+            if int(Y_hat[i]) != int(Y[i]):
+                error += 1
+                break
+        #     error += 1. - Y_hat[i].eq(Y[i].float).cpu().float().mean().item()
 
-        return error, Y_hat
+        return error, Y_prob, Y_hat
 
     def calculate_objective(self, X, Y):
         # Y = Y.float()
-        Y_prob, _, A = self.forward(X)
-        # Y_prob = torch.clamp(Y_prob, min=1e-5, max=1. - 1e-5)
-        neg_log_likelihood = 0
-        for i in range(10):
-            Y_prob[i] = torch.clamp(Y_prob[i], min=1e-5, max=1. - 1e-5)
-            neg_log_likelihood += -1. * (Y[i].float * torch.log(Y_prob[i]) + (1. - Y[i].float) * torch.log(1. - Y_prob[i]))  # negative log bernoulli
-
-        return neg_log_likelihood, A
+        Y = Y.float()
+        loss = nn.BCELoss(reduction ='sum')
+        Y_prob = self.forward(X)
+        Y_prob = torch.clamp(Y_prob, min=1e-5, max=1. - 1e-5)
+        # print(Y_prob)
+        # print(Y)
+        output = loss(Y_prob, Y)
+        print(output.item())
+        return output
